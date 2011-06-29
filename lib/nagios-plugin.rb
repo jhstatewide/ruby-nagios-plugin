770161c24aa1c47885db94ad266e61a76256ed62
Joshua Harding <josh@statewidesoftware.com>
2011-06-29 16:13:48 -0400

    initial commit

---
+++ lib/nagios-plugin.rb
@@ -0,0 +1,87 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module Nagios
+  class Plugin
+     VERSION = '0.0.1'
+     OK_STATE = 0
+     WARNING_STATE = 1
+     CRITICAL_STATE = 2
+     UNKNOWN_STATE = 3
+     
+     def initialize(args = {})
+     	 @shortname = args[:shortname] || "Nagios::Plugin"
+     	 @perfdata = Hash.new     
+     	 @exit = true
+     end
+     
+     def exit=(enabled)
+     	     @exit = enabled
+     end
+     
+     # actually does the check
+     def run
+     	     raise "You must implement the run method!"
+     end
+     
+     def state 
+     	   @state
+     end
+     
+     def output 
+     	     @output
+     end
+     
+     def add_perfdata(args)
+     	   field = args[:field] or raise "must supply field!"
+     	   data = args[:data] or raise "must supply data!"
+     	   @perfdata[field] = {:data => data}
+     	   @perfdata[field][:uom] = args[:uom] if args[:uom]
+     	   @perfdata[field][:warning] = args[:warning] if args[:warning]
+     	   @perfdata[field][:critical] = args[:critical] if args[:critical]
+     end
+     
+     private
+     
+     def critical(message)
+     	     @state = :CRITICAL
+     	     do_exit(CRITICAL_STATE, message)
+     end
+     
+     def ok(message)
+     	     @state = :OK
+     	     do_exit(OK_STATE, message)
+     end
+     
+     def warning(message)
+     	     @state = :WARNING
+     	     do_exit(WARNING_STATE, message)
+     end
+     
+     def do_exit(status, message)
+     	     @output = @shortname + " "
+     	     case status
+     	     when OK_STATE
+     	     	     @output += "OK - "
+     	     when WARNING_STATE
+     	     	     @output += "WARNING - "
+     	     when CRITICAL_STATE
+     	     	     @output += "CRITICAL - "
+     	     when UNKNOWN_STATE
+     	     	     @output += "UNKNOWN -"
+     	     end
+     	     @output += message
+     	     # see if we have keys...
+     	     if @perfdata.keys.length > 0
+     	     	     @output += " | "
+     	     	     @perfdata.each_pair do |key, value|
+     	     	     	     @output += "#{key}=#{value[:data]}#{value[:uom]};#{value[:warning]};#{value[:critical]}"  
+     	     	     end
+     	     end
+     	     exit(status) if @exit
+     	     @output.strip!
+     	     return @output
+     end
+     
+  end 
+end
---
+++ test/test_nagios-plugin.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+
+class TestPlugin < Nagios::Plugin
+	def run
+	    critical("waffles are getting really low...")
+	end
+end
+
+class TestNagiosPlugin < Test::Unit::TestCase
+
+  def setup
+  	  @tp = TestPlugin.new(:shortname => "Waffles")
+  	  @tp.exit = false
+  end
+  
+  def test_critical
+  	  output = @tp.run
+  	  assert_equal("Waffles CRITICAL - waffles are getting really low...", output)
+  end
+  
+  def test_perf
+  	  @tp.add_perfdata(:field => "waffles_per_second", :data => 10, :warning => 2, :critical => 5, :uom => "wps")
+  	  @tp.run
+  	  output = @tp.output
+  	  assert_equal("Waffles CRITICAL - waffles are getting really low... | waffles_per_second=10wps;2;5", output)
+  end
+end
